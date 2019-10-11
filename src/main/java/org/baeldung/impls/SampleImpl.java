a04dd444e4d7a5bbae7ed83bedc5d13501b44482
gaurav.verma01 <gaurav.verma01@BLRKEC402696D.ad.infosys.com>
2019-10-11 15:58:23 +0530

    Adding Sonar in Build.

---
+++ src/main/java/org/baeldung/impls/SampleImpl.java
@@ -0,0 +1,16 @@
+package org.baeldung.impls;
+
+public class SampleImpl {
+
+	//This method just prints data on console.
+	public void printData() {
+		System.out.println("RANDOM_VAL");
+	}
+	
+	/*
+	 * this is an empty method
+	 */
+	public void saveData() {
+		
+	}
+}
